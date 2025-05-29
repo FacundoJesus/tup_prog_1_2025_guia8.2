070a03c36000ce2cb3db2312e884db1c7a571f90
FacuJesus <facundojesus10@hotmail.com>
2025-05-29 19:03:51 -0300

    Encuesta de uso de transporte

---
+++ Guia 8.2/Ejercicio1/Models/Servicio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    internal class Servicio
+    {
+        public int cantidadEncuestados = 0;
+
+        static int contadorBici = 0;
+        static int contadorMoto = 0;
+        static int contadorAuto = 0;
+        static int contadorPublico = 0;
+
+        static double acumuladorDistanciaBici = 0;
+        static double acumuladorDistanciaMoto = 0;
+        static double acumuladorDistanciaAuto = 0;
+        static double acumuladorDistanciaPublico = 0;
+
+
+        public void RegistrarEncuesta(int tipoTransporte, double distancia)
+        {
+            switch (tipoTransporte)
+            {
+                case 1:
+                    contadorBici++;
+                    acumuladorDistanciaBici += distancia;
+                    break;
+                case 2:
+                    contadorMoto++;
+                    acumuladorDistanciaMoto += distancia;
+                    break;
+                case 3:
+                    acumuladorDistanciaAuto += distancia;
+                    contadorAuto++;
+                    break;
+                case 4:
+                    contadorPublico++;
+                    acumuladorDistanciaPublico += distancia;
+                    break;
+            }
+            if (tipoTransporte > 0 && tipoTransporte < 5)
+            {
+                cantidadEncuestados++;
+            }
+        }
+        public void CalcularPromedioPorTipo()
+        {
+
+            double promedioDistanciaBici = acumuladorDistanciaBici / contadorBici;
+            double promedioDistanciaMoto = acumuladorDistanciaMoto / contadorMoto;
+            double promedioDistanciaAuto = acumuladorDistanciaAuto / contadorAuto;
+            double promedioDistanciaPublico = acumuladorDistanciaPublico / contadorPublico;
+            Console.WriteLine($"Promedio de Distancia Recorrida en bici: {promedioDistanciaBici:f2} km");
+            Console.WriteLine($"Promedio de Distancia Recorrida en Moto: {promedioDistanciaMoto:f2} km");
+            Console.WriteLine($"Promedio de Distancia Recorrida en Auto: {promedioDistanciaAuto:f2} km");
+            Console.WriteLine($"Promedio de Distancia Recorrida en T.Público: {promedioDistanciaPublico:f2} km");
+
+
+            Console.WriteLine("No hubo registro de encuestados.");
+
+        }
+    }
+}
---
+++ Guia 8.2/Ejercicio1/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.ComponentModel;
+using Ejercicio1.Models;
+namespace Ejercicio1
+{
+
+    internal class Program
+    {
+
+        static Servicio servicio = new Servicio();
+        
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("---MENU---");
+            Console.WriteLine("Elija la opcion deseada:");
+            Console.WriteLine("1 - Iniciar encuesta (inicializar variables)");
+            Console.WriteLine("2 - Registrar una encuesta ");
+            Console.WriteLine("3 - Registrar varias encuestas");
+            Console.WriteLine("4 - Mostrar los promedios en distancia recorrida por cada tipo de transporte (tipo de transporte -1)");
+            Console.WriteLine("5 - Mostrar el total de encuestados");
+            Console.WriteLine("-1 Para Salir de la Aplicación... ");
+            int opt = Convert.ToInt32(Console.ReadLine());
+            return opt;
+        }
+
+        static void MostrarPantallaInicializarVariables()
+        {
+            Console.Clear();
+            servicio = new Servicio();
+            Console.WriteLine("Variables inicializadas.");
+            Console.WriteLine("\nPresione Enter para volver al Menu Principal");
+            Console.ReadKey();
+        }
+        static void MostrarPantallaSolicitarEncuesta()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el tipo de vehículo o (-1 Para volver al Menú Principal):");
+            Console.WriteLine("1 - Bicicleta");
+            Console.WriteLine("2 - Motocicleta");
+            Console.WriteLine("3 - Automóvil");
+            Console.WriteLine("4 - Transporte Público");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            if (opcion > 0 && opcion < 5)
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese la distancia recorrida:");
+                double distanciaRecorrida = Convert.ToDouble(Console.ReadLine());
+                servicio.RegistrarEncuesta(opcion, distanciaRecorrida);
+            }
+            else
+            {
+                Console.WriteLine("Tipo de transporte no  válido");
+            }
+            Console.WriteLine("\nPresione Enter para volver al Menu Principal");
+            Console.ReadKey();
+        }
+        static void MostrarPantallSolicitarVariasEncuestas()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese el tipo de vehículo o -1 Para volver al Menú Principal");
+            Console.WriteLine("1 - Bicicleta");
+            Console.WriteLine("2 - Motocicleta");
+            Console.WriteLine("3 - Automóvil");
+            Console.WriteLine("4 - Transporte Público");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            if (opcion <= 0 || opcion >= 5)
+            {
+                Console.WriteLine("Tipo de transporte no válido");
+            }
+            else
+            {
+                while (opcion != -1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Ingrese la distancia recorrida:");
+                    double distanciaRecorrida = Convert.ToDouble(Console.ReadLine());
+                    servicio.RegistrarEncuesta(opcion, distanciaRecorrida);
+                    Console.WriteLine("Ingrese el tipo de vehículo o -1 Para volver al Menú Principal");
+                    Console.WriteLine("1 - Bicicleta");
+                    Console.WriteLine("2 - Motocicleta");
+                    Console.WriteLine("3 - Automóvil");
+                    Console.WriteLine("4 - Transporte Público");
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+                
+            Console.WriteLine("\nPresione Enter para volver al Menu Principal");
+            Console.ReadKey();
+        }
+        static void MostrarPantallaPromediosResultados()
+        {
+            Console.Clear();
+            if (servicio.cantidadEncuestados > 0)
+            {
+                servicio.CalcularPromedioPorTipo();
+                
+            }
+            else
+            {
+                Console.WriteLine("No hubo encuestados");
+                
+            }
+            Console.WriteLine("\nPresione Enter para volver al Menu Principal");
+            Console.ReadKey();
+
+        }
+        static void MostrarPantallaTotalEncuestados()
+        {
+
+            Console.Clear();
+            if (servicio.cantidadEncuestados > 0)
+            {
+                Console.WriteLine($"Cantidad Total de encuestados: {servicio.cantidadEncuestados}");
+            }
+            else
+            {
+                Console.WriteLine("No hubo encuestados");
+            }
+
+            Console.WriteLine("\nPresione Enter para volver al Menu Principal");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            int opcion = MostrarPantallaSolicitarOpcionMenu();
+            while (opcion != -1)
+            {
+                switch(opcion) {
+                    case 1:
+                        MostrarPantallaInicializarVariables();
+                        break;
+                    case 2:
+                        MostrarPantallaSolicitarEncuesta();
+                        break;
+                    case 3:
+                        MostrarPantallSolicitarVariasEncuestas();
+                        break;
+                    case 4:
+                        MostrarPantallaPromediosResultados();
+                        break;
+                    case 5:
+                        MostrarPantallaTotalEncuestados();
+                        break;
+                    default:
+                        opcion = -1;
+                        break;
+                }
+
+                if (opcion != -1)
+                {
+                    opcion = MostrarPantallaSolicitarOpcionMenu();
+                }
+            }
+        }
+    }
+}
